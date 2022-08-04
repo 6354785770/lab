be5304016a0bdaf7d0f2eafb12f0161a599d3b1c
Ramchandra <109593381+6354785770@users.noreply.github.com>
2022-08-04 04:28:44 -0700

    Add files via upload

---
+++ Matrix Transpose.java
@@ -0,0 +1,29 @@
+public class MatrixTranspose{
+ public static void main(String args[]){  
+
+int original[][]={{1,2,3},{5,6,7},{0,2,4}};    
+    
+int transpose[][]=new int[3][3];  
+    
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+transpose[i][j]=original[j][i];  
+}    
+}    
+  
+System.out.println(" Matrix without transpose:");  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+System.out.print(original[i][j]+" ");    
+}    
+System.out.println();    
+}    
+System.out.println(" Matrix After Transpose:");  
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+System.out.print(transpose[i][j]+" ");    
+}    
+System.out.println();  
+}    
+}}  
+
